8f4cd8dffafc18228dfa1e05ca26d046382be935
AndreaVk <vico.ac1996@gmail.com>
2022-02-15 20:13:39 +0100

    organizzazione cartelle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <span>Benvenuto {{ Auth::user()->name }}, hai effettuato il login correttamente!</span>
                 </div>
             </div>
         </div>
