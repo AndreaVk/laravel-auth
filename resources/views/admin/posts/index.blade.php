558ad88db8d5d632ecd423241e81139fbf3df526
AndreaVk <vico.ac1996@gmail.com>
2022-02-16 18:42:29 +0100

    create

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,11 +8,32 @@
                 <div class="card-header">Post List</div>
 
                 <div class="card-body">
-                    <ul>
+                    <div class="mb-3">
+                        <a href="{{route("posts.create")}}"><button type="button" class="btn btn-info">Aggiungi Post</button></a>
+                    </div>
+                    <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
                         @foreach ($posts as $post)
-                        <li>{{$post->title}}</li>
+                            <tr>
+                                <td>{{$post->id}}</td>
+                                <td>{{$post->title}}</td>
+                                <td>{{$post->slug}}</td>
+                                <td>
+                                <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-outline-primary">Visualizza</button></a>
+                                </td>
+                            </tr>
                         @endforeach
-                    </ul>
+                    </tbody>
+                    </table>
                 </div>
             </div>
         </div>
